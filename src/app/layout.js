e9137d48e3a88f3872a220acabdf3b2300c90d8f
habib-676 <rahman.habibur2421k@gmail.com>
2025-08-27 12:44:22 +0600

    connect mongodb

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,9 @@ export default function RootLayout({ children }) {
           <Navbar></Navbar>
         </div>
         {children}
+        <div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center py-6 mt-auto">
+      <p>© {new Date().getFullYear()} ProductApp. All rights reserved.</p>
+    </footer>
+  );
+}
